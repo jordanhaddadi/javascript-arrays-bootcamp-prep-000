383cb2b609e976bb06a630f7664c927fa611b00e
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-23 02:33:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,5 +39,9 @@ var chocolateBars = [
     return array.slice(1);
   }
   
-  
+  function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.slice();
+    return array;
+  }
+    
   
