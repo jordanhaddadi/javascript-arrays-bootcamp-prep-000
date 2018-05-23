685d837bc4dd676cec2c2c52810daeed6d7ee838
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-23 02:09:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,15 +4,15 @@ var chocolateBars = [
   "skittles"];
   
   function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
   
   function destructivelyAddElementToBeginningOfArray(array,element){
-    array.unshift(element)
-    return array
+    array.unshift(element);
+    return array;
   }
   
   function addElementToEndOfArray(array, element) {
-    array.push(element)
-    return array
+    array.push("element");
+    return array;
   }
